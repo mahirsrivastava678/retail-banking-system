ac5eb59849732e475efe17f3a65e39357cdd5a90
samimahir <mahirshrivastava2@gmail.com>
2026-04-19 21:02:07 +0530

    init commit

--- src/main/java/com/mahir/retailbanking/repository/UserRepository.java
+++ src/main/java/com/mahir/retailbanking/repository/UserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mahir.retailbanking.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     User findByEmail(String email);
+
+    // ✅ ADD THIS
     User findByAccountNumber(String accountNumber);
 }
